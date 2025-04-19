257355e47827ed4af2ea5acf353201e214b880f2
Fedor Ezeev <fezeev@gmail.com>
2025-04-19 23:29:59 +0300

    some cosmetics improvements

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv("1834_kazan.csv")
-st.set_page_config(layout="wide")
-st.title("Historical Data Viewer")
-#st.dataframe(df)
-
+st.set_page_config(page_title="Список кантонистов", layout="wide")
+st.title("Список кантонистов")
 search = st.text_input("Search")
 filtered_df = df[df.apply(lambda row: row.astype(str).str.contains(search, case=False).any(), axis=1)]
 st.dataframe(filtered_df)
